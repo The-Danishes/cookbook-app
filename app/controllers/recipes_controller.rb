4f1cf8571576f81ab730f56c64a9abdc4ee91226
Danish Mughal <mughal.danish@gmail.com>
2016-11-16 18:38:09 -0800

    Added redirect and flash messages

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -14,6 +14,10 @@ class RecipesController < ApplicationController
   def create
     @recipe = Recipe.new(title: params[:title], chef: params[:chef], ingredients: params[:ingredients], directions: params[:directions])
     @recipe.save
+
+    flash[:success] = "Recipe has been created!"
+
+    redirect_to "/recipes/#{@recipe.id}"
   end
 
   def edit
@@ -24,6 +28,19 @@ class RecipesController < ApplicationController
     recipe = Recipe.find_by(id: params[:id])
     recipe.assign_attributes(title: params[:title], chef: params[:chef], ingredients: params[:ingredients], directions: params[:directions])
     recipe.save
+
+    flash[:success] = "You have edited the recipe."
+
+    redirect_to "/recipes/#{recipe.id}"
+  end
+
+  def destroy
+    recipe = Recipe.find_by(id: params[:id])
+    recipe.destroy
+
+    flash[:danger] = "Recipe has been deleted!" 
+
+    redirect_to "/recipes"
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,7 @@ Rails.application.routes.draw do
 
   get "/recipes/:id", to: "recipes#show"
 
-
-
+  delete "/recipes/:id", to: "recipes#destroy"
 
 
 
