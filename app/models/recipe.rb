d77730fa93cc98df376485c92dbd8e602c5c5b34
Danish Mughal <mughal.danish@gmail.com>
2016-11-17 00:48:26 -0800

    Added styling + basic model methods

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,15 @@
 class Recipe < ApplicationRecord
+
+  def ingredients_list
+    return ingredients.split(", ")
+  end
+
+  def directions_list
+    return directions.split(", ")
+  end
+
+  def pretty_time
+    return created_at.strftime("%A, %d %b %Y %l:%M %p")
+  end
+
 end
